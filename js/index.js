bee4ed52edf08730d65a4c2863a77f74bf891619
Vasiliy Pupkin <vasya_p@mail.com>
2020-02-16 15:25:17 +0200

    feature/lesson2:created variables with different values and displayed them in alert

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let name = 'My name Dmytro\n';
+let familyName = 'My family Marushchak\n';
+let years = 'I am ' + 23 + ' years old\n';
+let messageForYou = 'Hello my friends\n';
+
+let allInfoForMy = name + familyName + years + messageForYou;
+
+alert(allInfoForMy);
