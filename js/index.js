ace6ab3785b2f3069da5c116394cb29b4a308293
Vasiliy Pupkin <vasya_p@mail.com>
2020-02-19 00:40:49 +0200

    feature/lesso3:made a task in which I indicate comments or wishes to the user in the use of alcohol

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-let name = 'My name Dmytro\n';
-let familyName = 'My family Marushchak\n';
-let years = 'I am ' + 23 + ' years old\n';
-let messageForYou = 'Hello my friends\n';
+"use strict";
+let name = prompt ('My name Dmytro\n');
+let familyName = prompt('My family Marushchak\n');
+let years =  prompt ('I am ' + 23 + ' years old\n');
+let messageForYou = prompt ('Hello my friends\n');
 
 let allInfoForMy = name + familyName + years + messageForYou;
 
